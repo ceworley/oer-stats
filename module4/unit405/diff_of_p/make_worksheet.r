aeffbd9ceff6e96969711b97269828bd560b877b
ceworley <ceworley@bhcc.mass.edu>
2019-04-22 22:42:45 -0400

    fixed pdiffs

--- module4/unit405/diff_of_p/make_worksheet.r
+++ module4/unit405/diff_of_p/make_worksheet.r
@@ -1,5 +1,8 @@
 library("exams")
 
+seed = 123
+set.seed(seed)
+
 probs = c("diff_p_sampling_left_prob.Rmd",
           "diff_p_sampling_right_prob.Rmd",
           "diff_p_sampling_central_prob.Rmd",
@@ -9,11 +12,9 @@ probs = c("diff_p_sampling_left_prob.Rmd",
           "diff_p_test.Rmd",
           "diff_p_test.Rmd")
             
-exams2pdf(probs, n = 1, name = "diff_p_worksheet",
+exams2pdf(probs, name = paste0("diff_p_worksheet_seed_",seed,"_version_",sep=""),
   encoding = "UTF-8",
   dir = "output",
-  template = "worksheet.tex",
-  header = list(
-    Date = "2015-01-01",
-    ID = function(i) formatC(i, width = 5, flag = "0")
-  ))
+  edir = "exercises",
+  template = "templates/worksheet.tex",
+  )
